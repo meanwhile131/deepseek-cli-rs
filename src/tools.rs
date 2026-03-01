7e17eea98d16cc377661ead65a6c83e7fff99109
Sasha Chernyakov <sashachernyakov111111@gmail.com>
2026-03-01 12:42:25 +0400

    Support headless browser if no DISPLAY env var set

--- src/tools.rs
+++ src/tools.rs
@@ -283,11 +283,12 @@ struct BrowserState {
 
 impl BrowserState {
     async fn new() -> Result<Self> {
+        let mut builder = BrowserConfig::builder();
+        if std::env::var("DISPLAY").is_ok() {
+            builder = builder.with_head();
+        }
         let (browser, handler) = Browser::launch(
-            BrowserConfig::builder()
-                .with_head()
-                .build()
-                .map_err(anyhow::Error::msg)?,
+            builder.build().map_err(anyhow::Error::msg)?,
         )
         .await?;
         let handler_task = tokio::spawn(handler.for_each(|_| async {}));
